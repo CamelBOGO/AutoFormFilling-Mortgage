f0aa1188564cf5f4c3b2a7b3ef90220f0f78693a
BOGO <42817357+CamelBOGO@users.noreply.github.com>
2025-04-19 22:01:30 +0800

    The correct way to use the library.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const { PDFDocument } = require("pdf-lib");
+// import { PDFDocument } from "pdf-lib"
 
 async function flattenForm() {
-    const formUrl = "https://jocason-housing.com/renovation/referral-form/referral-form.pdf";
+    const formUrl = "https://jocason-housing.com/renovation/referral-form/referral-form.pdf"
     const formPdfBytes = await fetch(formUrl).then(res => res.arrayBuffer());
 
-    const pdfDoc = await PDFDocument.load(formPdfBytes);
+    const pdfDoc = await PDFLib.PDFDocument.load(formPdfBytes);
 
     const form = pdfDoc.getForm();
 
@@ -18,5 +18,5 @@ async function flattenForm() {
 flattenForm().then(() => {
     console.log("PDF Form Flattened and Saved");
 }).catch(err => {
-    console.error("Error flattening PDF form:", err);
-});
+    console.error("Error flattening PDF form:", err)
+})
