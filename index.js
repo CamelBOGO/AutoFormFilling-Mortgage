efe4d421a08ee71a575ecaad5ced6612370122ba
BOGO <42817357+CamelBOGO@users.noreply.github.com>
2025-04-19 21:49:22 +0800

    Change import to require.

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-import { PDFDocument } from "pdf-lib"
+const { PDFDocument } = require("pdf-lib");
 
 async function flattenForm() {
-    const formUrl = "https://jocason-housing.com/renovation/referral-form/referral-form.pdf"
-    const formPdfBytes = await fetch(formUrl).then(res => res.arrayBuffer())
+    const formUrl = "https://jocason-housing.com/renovation/referral-form/referral-form.pdf";
+    const formPdfBytes = await fetch(formUrl).then(res => res.arrayBuffer());
 
-    const pdfDoc = await PDFDocument.load(formPdfBytes)
+    const pdfDoc = await PDFDocument.load(formPdfBytes);
 
-    const form = pdfDoc.getForm()
+    const form = pdfDoc.getForm();
 
     form.getTextField("fill_10").setText("Some Text Here!!!");
 
     form.flatten();
 
-    const pdfBytes = await pdfDoc.save()
+    const pdfBytes = await pdfDoc.save();
 }
 
 flattenForm().then(() => {
-    console.log("PDF Form Flattened and Saved")
+    console.log("PDF Form Flattened and Saved");
 }).catch(err => {
-    console.error("Error flattening PDF form:", err)
-})
+    console.error("Error flattening PDF form:", err);
+});
