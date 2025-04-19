011cb405a3391e8ef1faacc77881621dc587951d
BOGO <42817357+CamelBOGO@users.noreply.github.com>
2025-04-19 21:47:12 +0800

    Test the PDF-LIB library.

---
+++ index.js
@@ -0,0 +1,22 @@
+import { PDFDocument } from "pdf-lib"
+
+async function flattenForm() {
+    const formUrl = "https://jocason-housing.com/renovation/referral-form/referral-form.pdf"
+    const formPdfBytes = await fetch(formUrl).then(res => res.arrayBuffer())
+
+    const pdfDoc = await PDFDocument.load(formPdfBytes)
+
+    const form = pdfDoc.getForm()
+
+    form.getTextField("fill_10").setText("Some Text Here!!!");
+
+    form.flatten();
+
+    const pdfBytes = await pdfDoc.save()
+}
+
+flattenForm().then(() => {
+    console.log("PDF Form Flattened and Saved")
+}).catch(err => {
+    console.error("Error flattening PDF form:", err)
+})
