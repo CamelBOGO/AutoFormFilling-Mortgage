28ea74c355e04b98783c8db99acaff3ecf7309a5
BOGO <42817357+CamelBOGO@users.noreply.github.com>
2025-04-20 19:39:32 +0800

    Test for basic function.

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
 // import { PDFDocument } from "pdf-lib"
 
 async function flattenForm() {
+    // Download the PDF form from the URL.
     const formUrl = "https://jocason-housing.com/renovation/referral-form/referral-form.pdf"
     const formPdfBytes = await fetch(formUrl).then(res => res.arrayBuffer());
-
     const pdfDoc = await PDFLib.PDFDocument.load(formPdfBytes);
-
+    // Get the form from the PDF document.
     const form = pdfDoc.getForm();
 
-    form.getTextField("fill_10").setText("Some Text Here!!!");
+    // Fill in the form fields with some values.
+    form.getTextField("fill_name_e_1").setText(document.getElementById("name_e_1").value);
+    form.getTextField("fill_id_1").setText(document.getElementById("hkid_1").value);
+    form.getTextField("fill_name_e_2").setText(document.getElementById("name_e_2").value);
+    form.getTextField("fill_id_2").setText(document.getElementById("hkid_2").value);
+    form.getTextField("fill_mobile").setText(document.getElementById("phone_1").value);
 
+    form.getTextField("fill_flat").setText(document.getElementById("room").value);
+    form.getTextField("fill_floor").setText(document.getElementById("floor").value);
+    form.getTextField("fill_block").setText(document.getElementById("block").value);
+    form.getTextField("fill_building").setText(document.getElementById("building").value);
+    form.getTextField("fill_district").setText(document.getElementById("district").value);
+
+    // Flatten and save the form to a new PDF file.
     form.flatten();
-
     const pdfBytes = await pdfDoc.save();
 
+    // Create a auto clicking download link for the new PDF file.
+    // This will trigger the browser's download dialog.
     const a = Object.assign(document.createElement("a"), {
         href: URL.createObjectURL(new Blob([pdfBytes], { type: "application/pdf" })),
-        download: "flattened_form.pdf"
+        download: "referral-form.pdf"
     });
     a.click();
 }
 
-flattenForm().then(() => {
-    console.log("PDF Form Flattened and Saved");
-}).catch(err => {
-    console.error("Error flattening PDF form:", err)
-})
+// flattenForm().then(() => {
+//     console.log("PDF Form Flattened and Saved");
+// }).catch(err => {
+//     console.error("Error flattening PDF form:", err)
+// })
